b7cebf84b032a2988688acd35bfe34737870462c
mirii1994 <miri.ignatiev@logz.io>
2020-10-14 12:11:11 +0300

    works on lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,3 @@
-import chromedriver_binary
 import datetime
 import json
 import os
@@ -144,11 +143,50 @@ def _get_driver():
         desired_capabilities['goog:loggingPrefs'] = {'browser': 'ALL', 'performance': 'ALL'}
         options = webdriver.ChromeOptions()
         # TODO: check for more options
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        # chromedriver_binary.add_chromedriver_to_path()
-        # options.binary_location = chromedriver_binary.chromedriver_filename
-        # driver = webdriver.Chrome(desired_capabilities=desired_capabilities, options=options, executable_path=chromedriver_binary.chromedriver_filename)
+        lambda_options = [
+            '--autoplay-policy=user-gesture-required',
+            '--disable-background-networking',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-breakpad',
+            '--disable-client-side-phishing-detection',
+            '--disable-component-update',
+            '--disable-default-apps',
+            '--disable-dev-shm-usage',
+            '--disable-domain-reliability',
+            '--disable-extensions',
+            '--disable-features=AudioServiceOutOfProcess',
+            '--disable-hang-monitor',
+            '--disable-ipc-flooding-protection',
+            '--disable-notifications',
+            '--disable-offer-store-unmasked-wallet-cards',
+            '--disable-popup-blocking',
+            '--disable-print-preview',
+            '--disable-prompt-on-repost',
+            '--disable-renderer-backgrounding',
+            '--disable-setuid-sandbox',
+            '--disable-speech-api',
+            '--disable-sync',
+            '--disk-cache-size=33554432',
+            '--hide-scrollbars',
+            '--ignore-gpu-blacklist',
+            '--ignore-certificate-errors',
+            '--metrics-recording-only',
+            '--mute-audio',
+            '--no-default-browser-check',
+            '--no-first-run',
+            '--no-pings',
+            '--no-sandbox',
+            '--no-zygote',
+            '--password-store=basic',
+            '--use-gl=swiftshader',
+            '--use-mock-keychain',
+            '--single-process',
+            '--headless']
+
+        for argument in lambda_options:
+            options.add_argument(argument)
+        options.binary_location = "/opt/bin/chromium"
         driver = webdriver.Chrome(desired_capabilities=desired_capabilities, options=options)
         return driver
     except Exception as e:
@@ -203,8 +241,15 @@ def _format_timestamp(timestamp):
     return "{}Z".format(timestamp.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3])
 
 
-_monitor("http://www.logz.io")
+def _get_url():
+    try:
+        return os.environ["URL"]
+    except KeyError:
+        _send_log("Error - no URL entered")
+        return {}
 
 
-# def lambda_handler(event, context):
-#     _monitor("https://www.logz.io")
+def lambda_handler(event, context):
+    url = _get_url()
+    if url:
+        _monitor(url)
